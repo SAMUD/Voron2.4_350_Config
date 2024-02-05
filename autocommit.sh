298144e341e794cba3f524a29403ab83f7fe641b
Samuel <samuel@kumquad.de>
2024-02-05 18:33:29 +0100

    Autocommit from 2024-02-05 18:33:29
    Klipper on commit: 96ab906
    
    Moonraker on commit: 3008a13
    
    Mainsail version: v2.9.1

--- autocommit.sh
+++ autocommit.sh
@@ -31,8 +31,8 @@ mainsail_folder=~/mainsail
 #fluidd_folder=~/fluidd
 
 ### The branch of the repository that you want to save your config
-### By default that is 'master'
-branch=master
+### By default that is 'main'
+branch=main
 
 #####################################################################
 #####################################################################
@@ -73,7 +73,7 @@ grab_version(){
 
 push_config(){
   cd $config_folder
-  git pull origin $branch
+  git pull origin $branch --no-rebase
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
